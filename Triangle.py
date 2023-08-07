aba9af64f692c71011223bdfb6bd917c389d97e1
Elaheh Aria <122198974+ElahahAria@users.noreply.github.com>
2023-08-07 22:32:55 +0330

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -2,7 +2,21 @@ a1 = float(input("enter side one : "))
 a2 = float(input("enter side two : "))
 a3 = float(input("enter side three : "))
 
-if (a1 < a2 + a3) and (a2 < a1 + a3) and (a3 < a2 + a1):
-    print("OK")
+
+max=max(a1,a2,a3)
+
+if max==a1:
+    if a2+a3>max :
+        print("OK")
+    else :
+        print("NOT OK")
+elif max==a2:
+    if a1+a3>max :
+        print("OK")
+    else :
+        print("NOT OK")
 else:
-    print("Not OK")
+    if a2+a1>max :
+        print("OK")
+    else :
+        print("NOT OK")
